b9c447b5e3abd97bd6657b7ed7d2349067d97f4e
Seungsik Choi <seungsch@c1r24s4.42gyeongsan.kr>
2026-03-20 17:38:49 +0900

    :sparkles: feet : Parsing Environment Variables and Implementing Utility Functions

--- include/type.hpp
+++ include/type.hpp
@@ -6,4 +6,10 @@
 #include <algorithm>
 #include <iostream>
 
+/**
+ * @brief 문자열(std::string)을 키와 값으로 가지는 맵 (주로 환경변수나 HTTP 헤더 저장용)
+ */
+typedef std::map<std::string, std::string> EnvMap;
+
+
 #endif
